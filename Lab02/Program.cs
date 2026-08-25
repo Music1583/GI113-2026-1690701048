ec0dc8eef051b23d870d60248224e1695c77b238
music <sorawan.song@bumail.net>
2026-08-25 08:05:04 +0700

    addlab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID : 1690701048
+ * Name       : ศรวัณ ส่งคุณธรรม
+ * Section    : 129A
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
