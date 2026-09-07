8f08383cfa3089ef3af9a7dff69e1c7013ab3495
music <sorawan.song@bumail.net>
2026-09-07 19:07:49 +0700

    เพิ่ม assignment 1 และตั้งชื่อตัวแปร

---
+++ Assignment01/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+* Student ID : 1690701048
+* Name       : Sorawan Songkhunnatam
+* Section    : 129A
+* No.        : 36
+* Course     : GI113 Computer Programming (GI)
+*/
+namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //game don't strave together
+            Console.WriteLine("_______STATUS BEEFALO_______"); // Beefalo คือมอนในเกมที่สามารถขี่ได้
+            const string Type = "Neutral Animal";
+            var GameName = "Don't strave together";
+            var difficulty = 's';
+            char teir = 's';
+            string Name= "Beefalo";
+            int hp = 1000;
+            float normalspeed = 7f;
+            double attackPower = 34.0;
+            bool istem = false;
+            bool isfriendly = true;
+
+        }
+    }
+}
