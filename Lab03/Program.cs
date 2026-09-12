cb187fe4d045067e60999d0a751de8ef3e225303
music <sorawan.song@bumail.net>
2026-09-12 10:16:29 +0700

    Update เลขที่บน header ใน lab03

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -2,7 +2,7 @@
 * Student ID : 1690701048
 * Name       : ศรวัณ ส่งคุณธรรม
 * Section    : 129A
-* No.        :
+* No.        : 36
 * Course     : GI113 Computer Programming (GI)
 */
 namespace Lab03
