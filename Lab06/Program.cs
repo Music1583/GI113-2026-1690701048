9dd12a461ab4fec80142f827a19a94520fb05e9a
Music1583 <sorawan.song@bumail.net>
2026-09-21 10:06:22 +0700

    เพิ่ม 1 - 4

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -11,7 +11,63 @@ namespace Lab06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1
+            int live = 0;
+            if (live == 0)
+            {
+                Console.WriteLine("Game over");
+            }
+            Console.WriteLine("continue runing");
+
+            //2
+            int coins = 80;
+            int price = 10;
+            if (coins >= price)
+            {
+                Console.WriteLine("Purchased");
+            }
+            else
+            {
+                Console.WriteLine("Not enough coins.");
+            }
+
+            //3
+            int score = 75;
+            if (score >= 90)
+            {
+                Console.WriteLine("Rank S");
+            }
+            else if (score >= 60) 
+            {
+                Console.WriteLine("Rank A");
+            }
+            else
+            {
+                Console.WriteLine("Rank B");
+            }
+
+            //4
+            bool hasKey = true;
+            Console.Write("your level (1-99) : ");
+            bool isok = int.TryParse(Console.ReadLine(), out int level);
+
+            if (!isok || level <= 1 || level > 99)
+            {
+                Console.WriteLine("Invalid level , plase try again");
+            }
+            else if (level >= 10 && hasKey)
+            {
+                Console.WriteLine("Boos floor unlock");
+            }
+            else if (level >= 5)
+            {
+                Console.WriteLine("The door open ");
+            }
+            else
+            {
+                Console.WriteLine("The door stays shut.");
+            }
+
         }
     }
 }
