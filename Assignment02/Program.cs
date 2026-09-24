f6f04aa3772ee837fbf0d5792543b05c956e9168
music <sorawan.song@bumail.net>
2026-09-25 00:59:12 +0700

    เพิ่ม title โรงตีเหล็ก

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -10,8 +10,14 @@ namespace Assignment02
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+            Console.WriteLine("The Forge");
+            Console.WriteLine("Special ore today");
+            Console.WriteLine("Notebook Smelting 0.17 / Salvage 0.28");
+            Console.WriteLine("Key 'S' for Smelt (Ore -> Ingot)");
+            Console.WriteLine("Key 'B' for Breakdown (Ingot -> Ore)");
+            
+            
         }
     }
 }
